4d436993732a6306eef22a61f22133b84373d7aa
Yash Gupta <yash8917gupta@gmail.com>
2021-09-19 23:23:59 +0530

    topper Student

---
+++ src/StuTest.java
@@ -0,0 +1,19 @@
+class Student{
+    String name;
+    float pr;
+
+    public Student(String name, float pr) {
+        this.name = name;
+        this.pr = pr;
+        System.out.println("The name of the stuednt is:"+name);
+        System.out.println("The percentage is:"+pr);
+    }
+}
+public class StuTest {
+    public static void main(String[] args) {
+        Student obj=new Student("Sachin",89f);
+
+    }
+
+    }
+
