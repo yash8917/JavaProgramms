7a0dfb53bc02a2c5d72b669999f6658f787735cb
Yash Gupta <yash8917gupta@gmail.com>
2021-09-22 19:32:47 +0530

    Revers of a string

---
+++ src/StringRiver.java
@@ -0,0 +1,8 @@
+public class StringRiver {
+    public static void main(String[] args) {
+        StringBuffer s=new StringBuffer("abcdef");
+        System.out.println(s);
+        s.reverse();
+        System.out.println(s);
+    }
+}
