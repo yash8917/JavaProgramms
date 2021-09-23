28f750faee147a42587069bc8dbc9e5893d7ef26
Yash Gupta <yash8917gupta@gmail.com>
2021-09-23 15:03:22 +0530

    passing array as an argument

--- src/Lab3.java
+++ src/Lab3.java
@@ -1,20 +1,47 @@
+//public class Lab3 {
+//    public static void main(String[] args) {
+//        System.out.println("WElcome");
+//        Stu obj1=new Stu(23,new int[]{33,23_000,34,44,45});
+//        obj1.display();
+//    }
+//}
+////passing array as an argument
+//class Stu{
+//    int Rrollno;
+//    int arr[];
+//
+//    public Stu(int Rrollno, int[] arr) {
+//        this.Rrollno = Rrollno;
+//        this.arr = arr;
+//    }
+//     public void display(){
+//        System.out.println(Rrollno);
+//        for (int res: arr) {
+//            System.out.println(res);
+//        }
+//    }
+//}
+
+// use of static
+
 public class Lab3 {
     public static void main(String[] args) {
         System.out.println("WElcome");
-        Stu obj1=new Stu(23,new int[]{33,23_000,34,44,45});
-        obj1.display();
+        Stu.Stu(23,new int[]{12,3,45,56});
+//        Stu obj1=new Stu(23,new int[]{33,23_000,34,44,45});
+//        obj1.display();
     }
 }
 //passing array as an argument
 class Stu{
-    int Rrollno;
-    int arr[];
+    static  int Rrollno;
+   static int arr[];
 
-    public Stu(int Rrollno, int[] arr) {
-        this.Rrollno = Rrollno;
-        this.arr = arr;
+    public static void Stu(int Rrollno1, int[] arr1) {
+        Rrollno = Rrollno1;
+        arr = arr1;
     }
-     public void display(){
+    public static void display(){
         System.out.println(Rrollno);
         for (int res: arr) {
             System.out.println(res);
