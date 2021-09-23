3bb6d65e62ef945fefa8ff6267a7e53c4829842b
Yash Gupta <yash8917gupta@gmail.com>
2021-09-23 20:24:58 +0530

    Two no are equal or not

---
+++ src/NoAreEqual.java
@@ -0,0 +1,13 @@
+public class NoAreEqual {
+    public static void main(String[] args) {
+        int num1, num2;
+        num1 = 15;
+        num2 = 15;
+        if (num1 == num2) {
+            System.out.println("They are equal");
+        } else {
+            System.out.println("They are not equal");
+        }
+    }
+}
+
