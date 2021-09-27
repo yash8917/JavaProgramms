95bbbf58d58a75105126ac0419f76f86d12f05c8
Yash Gupta <yash8917gupta@gmail.com>
2021-09-27 21:11:05 +0530

    instanceof exam

---
+++ src/InstanceOfExa.java
@@ -0,0 +1,8 @@
+public class InstanceOfExa {
+
+        public static void main(String args[]){
+            InstanceOfExa s=new InstanceOfExa();
+            System.out.println(s instanceof InstanceOfExa);//true
+        }
+    }
+
