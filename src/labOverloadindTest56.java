2536e2f5e734dae265ba9480478ae10d94a4b5bc
Yash Gupta <yash8917gupta@gmail.com>
2021-10-07 18:09:44 +0530

    initial commit.

--- src/labOverloadindTest56.java
+++ src/labOverloadindTest56.java
@@ -1,16 +1,37 @@
+//class i1{
+//
+//}
+//class i2 extends i1{
+//
+//}
+//class i3 extends i2{
+//
+//}
+interface i1{
+
+}
+interface i2 extends i1{
+
+        }
+        interface i3 extends i1{
+
+        }
 class OverDemo{
 
-    public void m1(String obj1){
+    public void m1(i1 obj1){
         System.out.println("gla");
     }
-    public void m1(Object obj1){
+    public void m1(i2 obj1){
         System.out.println("hello");
+    }
+    public void m1(i3 obj1){
+        System.out.println(" welcome");
     }
 }
 public class labOverloadindTest56 {
     public static void main(String[] args) {
         // create a anonymous obj
-        new OverDemo().m1(null);
+        //new OverDemo().m1(null);
         OverDemo obj5=null;
     }
 }
---
+++ src/labTest890.java
@@ -0,0 +1,15 @@
+class parent{
+    public static void m1(){
+        System.out.println("ok");
+    }
+}
+class child extends parent{
+    public static void m1(){
+        System.out.println("welcome");
+    }
+}
+public class labTest890 {
+    public static void main(String[] args) {
+        child.m1();
+    }
+}
