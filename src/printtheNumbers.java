b7a18835bfed0f6fd57ab253f71231471d2c48da
Yash Gupta <yash8917gupta@gmail.com>
2021-10-16 19:32:43 +0530

    initial commit.

--- src/printtheNumbers.java
+++ src/printtheNumbers.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 public class printtheNumbers {
 
     public static void main(String[] args) {
-
         Scanner in = new Scanner(System.in);
 
         System.out.print("Input a number: ");
